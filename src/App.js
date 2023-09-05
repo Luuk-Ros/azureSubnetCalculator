f0f19619f8c2bfbc2db429cd0bb4421449e0c0b2
Luuk <luuk.ros@axians.com>
2023-09-05 10:49:05 +0200

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Azure VNET Subnet Calculator</h1>
+      <button>Calculate New VNET</button>
+      <button>Add Subnet to Existing VNET</button>
+    </div>
+  );
+}
+
+export default App;
